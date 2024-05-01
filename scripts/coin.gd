c126d4d75a1a954e1148bf87f62b6f9ea6d56624
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-05-01 15:42:54 +1000

    Added coin sound effects

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
+@onready var animator = $AnimationPlayer
 
 func _on_body_entered(body):
 	game_manager.add_points()
-	queue_free()
+	animator.play("pickup")
